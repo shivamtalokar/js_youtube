437addb529ae27b4bd69a1d50defe499221a6ee5
shivamtalokar <129674421+shivamtalokar@users.noreply.github.com>
2024-06-24 18:48:08 +0000

    conversion of data types

---
+++ basic_01/01_variables.js
@@ -0,0 +1,8 @@
+const accId = 12345
+let accName = "Shivam"
+var accPass = "234"
+accCity = "Daryapur"
+
+// accId = 23 not allowed
+
+console.log(accId)
---
+++ basic_01/conversion_and_operation.js
@@ -0,0 +1,27 @@
+let score = "Shivam"
+// console.log(typeof score)
+// console.log(score)
+
+let num_score = Number(score)
+// console.log(typeof num_score)
+// console.log(num_score)
+
+/*
+"33" => 33
+"33abc" => NaN 
+"true" =>1 , "False" =>0
+*/
+let loged = "Shiv"
+let boolLoged = Boolean(loged)
+
+console.log(boolLoged)
+
+/*
+1 =>true ,0=> false
+""=>false 
+"Shiv" =>true
+*/
+let num = 34
+let string_num = String(num)
+console.log(string_num)
+console.log(typeof string_num)
