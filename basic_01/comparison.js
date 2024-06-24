28d77ea4bea0996fd02db8401ce4b58ee4ee19a9
shivamtalokar <129674421+shivamtalokar@users.noreply.github.com>
2024-06-24 19:15:26 +0000

    comparison

---
+++ basic_01/comparison.js
@@ -0,0 +1,10 @@
+// console.log(2>2);
+// console.log(2>=2);
+// console.log(2<=2);
+// console.log(2==2);
+
+console.log("2">1); // "2" gets converted into number so true
+
+console.log(null>0);// false, null gets converted in number means 0 so, 0>0 = false
+console.log(null == 0) // false, equality chedck operator doesnt convert null into 0 
+console.log(null>=0) // true , > it converts null into 0 then 0==0 is true 
