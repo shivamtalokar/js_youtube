af78bac4afffe278024445286472d43196dfcb56
shivamtalokar <129674421+shivamtalokar@users.noreply.github.com>
2024-06-24 19:30:32 +0000

    data_type_summery

---
+++ basic_01/data_type_summery.js
@@ -0,0 +1,6 @@
+//primitive :
+    // 7 types : String, Boolean, null ,undefined, Number, Symbol, BigInt
+    //hya value copy hotat mag apann yachat change karto 
+
+//Referance type(Non primitive data type):
+    //Array, Object, Functions
