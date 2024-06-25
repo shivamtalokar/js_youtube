6ce28be9b303ac4e48b9882c60842525ea11a0d4
shivamtalokar <129674421+shivamtalokar@users.noreply.github.com>
2024-06-25 09:01:01 +0000

    math_num

---
+++ basic_01/06_nums_and_math.js
@@ -0,0 +1,22 @@
+// initialize 
+const num = 400
+
+const num2 = new Number("400")
+// console.log(num2);
+
+// methods 
+// console.log(num.toString());
+// console.log(num.toFixed(2)); // jas 400 ahew tar 400.00 karan fixed madhe 2 ahe tar .00 
+ 
+
+// ++++++++++++++++++++++++++++++++ MATH ++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math)
+console.log(Math.round(4.5))// round of karte value
+console.log(Math.abs(-5)); // minus value +ve karte
+console.log(Math.ceil(5.6))
+console.log(Math.random());//0 to 1 chya madhat value dete
+
+const min = 1 //jar value kashchya madhat paheje asel tar ha formula 
+const max= 6
+console.log(Math.floor(Math.random()*(max-min +1)+min)); // math.floor n jar 5.9 asel tar 5 milel max-min +1 yasthi ki value zero nahi aali panhije 
