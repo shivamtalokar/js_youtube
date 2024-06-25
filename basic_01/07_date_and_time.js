75fde93febbdd77a79291309800483ca461dd9e8
shivamtalokar <129674421+shivamtalokar@users.noreply.github.com>
2024-06-25 09:44:02 +0000

    date_time

---
+++ basic_01/07_date_and_time.js
@@ -0,0 +1,15 @@
+const date = new Date() // date object ahe
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleString);
+// console.log(date.toJSON());
+// console.log(date.toISOString());
+
+const my_date_create = new Date (2024,0,17,5,24) // yan new date create keli aani months 0pasun suru hotat anni 5 ha time ahe
+// console.log(my_date_create.toLocaleString());
+
+let my_time =Date.now()
+let my_creaeted = new Date("2024-06-25")
+console.log(my_time);
+console.log(my_creaeted.getTime()); // hya ch output 1 jan 1970 paun che milisec aste
