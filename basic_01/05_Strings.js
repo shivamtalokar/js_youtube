be08c960c16bbe6e3872c12d3dc15e93919decce
shivamtalokar <129674421+shivamtalokar@users.noreply.github.com>
2024-06-25 08:32:31 +0000

    today work

---
+++ basic_01/05_Strings.js
@@ -0,0 +1,21 @@
+// initialize
+
+const name = "Shivam1"
+
+const name2 = new String ("Shivam2")
+// console.log(name2);
+
+// concatinate
+ 
+// console.log("Shivam " + "Talokar" )
+//  console.log(`${"Shivam "}${"Talokar"}`) // we use this
+
+ // methods in strings 
+//  console.log(name.charAt(2))
+//  console.log(name.indexOf("S"))
+//  console.log(name.substring(0,4))
+ console.log(name.slice(-7, 7))
+ console.log(name.replace('h',''))
+   
+ const game = "    Snake    "
+ console.log(game.trim()) 
